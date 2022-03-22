7ba1d37b83e9aed204e57e5493edf3f9ddb21d4f
dawbru <kleofashurfa@gmail.com>
2022-03-22 15:16:22 +0100

    New test

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -9,7 +9,7 @@ import pages.*;
 import utilis.Interactions;
 
 public class BaseTest {
-    protected static final String BASE_URL = "http://automationpractice.com/index.php";
+    protected final String BASE_URL = "http://automationpractice.com/index.php";
     protected WebDriver driver;
     protected HomePage homePage;
     protected Interactions interactions;
